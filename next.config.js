906ed4e8221e8cd0cb0a9d70a74eba430895bd27
EKIM <ekim@EKIMui-Macmini.phub.net.cable.rogers.com>
2023-12-14 02:58:53 -0500

    add referrer

--- next.config.js
+++ next.config.js
@@ -14,7 +14,8 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,POST,PUT,OPTIONS" },
-                    { key: "Access-Control-Allow-Headers", value: "Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+                    { key: "Access-Control-Allow-Headers", value: "Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    { key: "Referrer Policy", value:'Origin-when-cross-origin'}
                 ]
             }
         ]
