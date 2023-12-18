71d6101e41fc7b94f92f6df7cf2e50448f92a738
eunbeek <50673489+eunbeek@users.noreply.github.com>
2023-12-18 03:29:05 -0500

    [#19] added changing password (#23)
    * disable using firebase auth with email method
    
    * change login response
    
    * add response status
    
    ---------
    
    Co-authored-by: EKIM <ekim@EKIMui-Macmini.phub.net.cable.rogers.com>

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,67 +1,77 @@
 import prisma from '@/app/lib/prisma';
-import firebase_app from "@/app/lib/firebase-config";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { verifyJwtToken } from '@/app/lib/auth';
+import * as bcrypt from 'bcrypt';
 
 interface RequestBody {
-  email: string;
-  password: string;
-  name: string;
-  platform: string;
+    email: string;
+    password: string;
+    name: string;
+    platform: string;
 }
 
-const auth = getAuth(firebase_app);
-
-// fetch user / all users
-export async function GET(request: Request) {
-  //token verified 
-  var token = request.headers.get('token');
-  if(!token) return new Response(JSON.stringify({status:400, error:'invalid token'}));
-  const reqUser = await verifyJwtToken(token);
-  if(!reqUser) return new Response(JSON.stringify({status:400, error:'invalid format token'}));
-
-  const { searchParams } = new URL(request.url);
-  const email = searchParams.get('email');
-  if(email === null) {
-    const users = await prisma.user.findMany();
-    return new Response(JSON.stringify(users));
-  } else {
-    const user = await prisma.user.findFirst({ where: {email: email, platform: 'ymmm'}});
-    return new Response(JSON.stringify(user));
-  }
-}
-
-// 1. Sign Up
 export async function POST(request: Request) {
-  const body: RequestBody = await request.json();
-  // 2. check email existing from postgressql
-  const users = await prisma.user.findMany({where: {email: body.email, platform: body.platform}});
-  if(users.length !== 0){
-    return new Response(JSON.stringify('Existing email, please use other email'));
-  }
-  
-  // 3. create user in firebase 
-  let result = null, error = null;
+    // check password or username change 
+    const { searchParams } = new URL(request.url);
+    const field = searchParams.get('field') === 'password'? true: false;
 
-  try {
-    result = await createUserWithEmailAndPassword(auth, body.email, body.password);
-    // 4. create user in postgressql match by uid
-    const user = await prisma.user.create({
-      data: {
-        uid: result?.user?.uid,
+    // check user existing
+    const body: RequestBody = await request.json();
+
+    const user = await prisma.user.findFirst({
+      where: {
         email: body.email,
-        name: body.name,
-        platform: body.platform,
+        platform: body.platform
       },
     });
-  } catch (e) {
-      error = e;
-      return new Response(JSON.stringify(error));
-  }
 
-  return new Response(JSON.stringify(result));
+    if(!user){
+      return new Response(JSON.stringify(null));
+    }
+
+    let updatedUser = null;
+
+    if(field){
+        try{
+            updatedUser = await prisma.user.update({
+                select:{
+                    email: true,
+                    name: true
+                },
+                where: {
+                    id: user.id
+                },
+                data:{
+                    password: await bcrypt.hash(body.password, 10),
+                }
+            });
+        }catch(e){
+            return new Response(JSON.stringify(e));
+        }
+    }else{
+        try{
+            var token = request.headers.get('token');
+            if(!token) return new Response(JSON.stringify({status:400, error:'invalid token'}));
+            const reqUser = await verifyJwtToken(token);
+            if(!reqUser) return new Response(JSON.stringify({status:400, error:'invalid format token'}));
+    
+            updatedUser = await prisma.user.update({
+                select:{
+                    email: true,
+                    name: true
+                },
+                where: {
+                    id: user.id
+                },
+                data:{
+                    name: body.name
+                }
+            });
+        }catch(e){
+            return new Response(JSON.stringify(e));
+        }
+    }
+    return new Response(JSON.stringify(updatedUser));
 }
-
 export async function OPTIONS(request: Request) {
-  return new Response(JSON.stringify({status: 200}));
+    return new Response(JSON.stringify({status: 200}));
 }
